8da4326dd05999a4b5cffa273e131093ef25f835
Farooq Aziz <50719873+farooqaziz20@users.noreply.github.com>
2025-06-11 18:47:18 +0500

    update

--- lib/global365_widgets.dart
+++ lib/global365_widgets.dart
@@ -6,3 +6,4 @@ export 'src/buttons/export_buttons.dart';
 export 'src/utils/export_utils.dart';
 export 'src/theme/theme_export.dart';
 export 'src/dropdowns/export_dropdowns.dart';
+export 'src/authentication/authentication_export.dart';
---
+++ lib/src/authentication/authentication_export.dart
@@ -0,0 +1,10 @@
+library;
+
+export 'signup/singup_screens/create_account.dart';
+export 'signup/singup_screens/verify_otp.dart';
+export 'signup/singup_screens/business_profile_setup.dart';
+export 'signup/singup_screens/finalize_signup.dart';
+export 'signup/singup_screens/set_up_screen.dart';
+export 'signup/singup_screens/software_info.dart';
+export 'signup/singup_screens/payment_info.dart';
+export 'signup/controllers/signup_controller/sign_up_controller.dart';
