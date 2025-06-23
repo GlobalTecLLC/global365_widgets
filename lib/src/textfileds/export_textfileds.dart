973c9123d077cb2224008afc459f2bc4009cf4aa
Saddam Hussain <105212372+Saddam7342@users.noreply.github.com>
2025-06-23 17:33:21 +0500

    added export number field.

--- lib/src/textfileds/export_textfileds.dart
+++ lib/src/textfileds/export_textfileds.dart
@@ -5,3 +5,6 @@ export 'decimal_text_input_formater.dart';
 export 'export_textfileds.dart';
 export 'my_text_field_for_multiple_line.dart';
 export 'my_text_field_single_line.dart';
+export 'my_customer_number_text_field.dart';
+export 'my_login_text_field.dart';
+export 'usphonenumer_filed.dart';
