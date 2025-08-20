612a99e164300bc2fd4f0423ea0ff216f96e3e90
Saddam Hussain <105212372+Saddam7342@users.noreply.github.com>
2025-08-20 14:05:04 +0500

    Updated radio button with text

--- lib/src/buttons/radio_button_with_text.dart
+++ lib/src/buttons/radio_button_with_text.dart
@@ -1,16 +1,9 @@
-
 import 'package:flutter/material.dart';
+import 'package:global365_widgets/global365_widgets.dart';
 import 'package:global365_widgets/src/constants/constants.dart';
-import 'package:global365_widgets/src/theme/text_widgets/text_variants/paragraph_normal.dart';
 
 class RadioButtonWithText extends StatelessWidget {
-  const RadioButtonWithText({
-    super.key,
-    required this.groupValue,
-    required this.value,
-    required this.text,
-    this.onChanged,
-  });
+  const RadioButtonWithText({super.key, required this.groupValue, required this.value, required this.text, this.onChanged});
 
   final dynamic groupValue;
   final dynamic value;
@@ -18,14 +11,27 @@ class RadioButtonWithText extends StatelessWidget {
   final void Function(dynamic)? onChanged;
   @override
   Widget build(BuildContext context) {
-    return Row(
-      children: [
-        SizedBox(
-          height: 10,
-          child: Radio(activeColor: primaryColor, value: value, groupValue: groupValue, onChanged: onChanged),
-        ),
-        GParagraphNormal(text, color: groupValue == value ? primaryColor : bodyText),
-      ],
+    return InkWell(
+      splashColor: Colors.transparent,
+      hoverColor: Colors.transparent,
+      highlightColor: Colors.transparent,
+      onTap: () => onChanged!(value),
+      child: Row(
+        children: [
+          SizedBox(
+            height: 10,
+            child: Radio(
+              splashRadius: 0,
+              hoverColor: Colors.transparent,
+              activeColor: primaryColor,
+              value: value,
+              groupValue: groupValue,
+              onChanged: onChanged,
+            ),
+          ),
+          GTextHeading6(text, color: groupValue == value ? primaryColor : bodyText),
+        ],
+      ),
     );
   }
 }
